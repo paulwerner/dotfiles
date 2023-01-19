c7b3be005fc3c87601e712e64112405e38eeb5b9
Paul Werner <contact@paulwerner.net>
2023-01-19 23:02:20 +0100

    add baseline

---
+++ scripts/generate_gitconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source "${ZSH}/script/shared/common.sh"
+
+generate_gitconfig() {
+    if ! [ -f git/gitconfig.symlink ]
+    then
+        info 'setup gitconfig'
+
+        user ' - What is your github author name?'
+        read -e git_authorname
+        user ' - What is your github author email?'
+        read -e git_authoremail
+
+        sed -e "s/AUTHORNAME/${git_authorname}/g" -e "s/AUTHOREMAIL/${git_authoremail}/g" git/gitconfig.template > git/gitconfig.symlink
+
+        success 'generated gitconfig'
+    else
+        info 'skipped gitconfig'
+        echo ''
+    fi
+}
